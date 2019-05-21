769d2f205f5fd65090495f7847409e48f3b767bf
Lizzie King <lizzieking521@gmail.com>
2019-05-21 13:55:49 +1000

    ADDED: axios and unirest for api calls

--- src/components/App.js
+++ src/components/App.js
@@ -8,40 +8,75 @@ import Card from "./Card/Card";
 import CardInfo from "./CardInfo/CardInfo";
 import SearchLocation from "./SearchLocation/SearchLocation";
 import SearchBar from "./SearchBar/SearchBar";
+import unirest from "unirest";
+import axios from 'axios';
 
 class App extends React.Component {
   // constructor(props) {
   //   super(props); //super is a reference to the parent constructor function
   //   this.state = { lat: null, errorMessage: '' }; //ONLY TIME TO DO THIS
   // }
-  state = { lat: null, errorMessage: "" };
+  state = { lat: null};
   componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position =>
-        //DO NOT EVER DO THIS this.state.lat = position.coords.latitude
-        this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
+    // window.navigator.geolocation.getCurrentPosition(
+    //   position =>
+    //     //DO NOT EVER DO THIS this.state.lat = position.coords.latitude
+    //     this.setState({ lat: position.coords.latitude }),
+    //   err => this.setState({ errorMessage: err.message })
+    // );
+
+    // unirest
+    //   .get(
+    //     "https://andruxnet-world-cities-v1.p.rapidapi.com/?query=paris&searchby=city"
+    //   )
+    //   .header("X-RapidAPI-Host", "andruxnet-world-cities-v1.p.rapidapi.com")
+    //   .header(
+    //     "X-RapidAPI-Key",
+    //     "951dd8fe7bmsh901d416187df458p18dbc5jsncd62ce4a5f58"
+    //   )
+    //   .end(function(result) {
+    //     console.log("testing>>", result.status, result.headers, result.body);
+    //     for (let i of result.body) {
+    //       console.log('i>>', i)
+    //       this.setState({ lat: i.city });
+    //     }
+  
+    //   });
+
   }
   componentDidUpdate() {
-    //commented calling this function out for now
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div> Error: {this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <section location={this.state.lat}> Latitude:</section>;
-    }
-    return (
-      <Spinner message="Please enable location tracking.. "> Loading! </Spinner>
-    );
+    return <section location={this.state.lat}> Latitude:</section>;
   }
+  // componentDidUpdate() {
+  //   //commented calling this function out for now
+  //   if (this.state.errorMessage && !this.state.lat) {
+  //     return <div> Error: {this.state.errorMessage}</div>;
+  //   }
+  //   if (!this.state.errorMessage && this.state.lat) {
+  //     return <section location={this.state.lat}> Latitude:</section>;
+  //   }
+  //   return (
+  //     <Spinner message="Please enable location tracking.. "> Loading! </Spinner>
+  //   );
+  // }
   onSearchSubmit(term) {
+    let path = 'search/photos'
+    let rootUrl = 'https://api.unsplash.com/'+ path;
+
+    axios.get(rootUrl, {
+      params: {
+        query: term
+      },
+      headers:{
+        Authorization: 'Client-ID 2e9ed63a98511c8e7500b2c474673945b9d2128d225ee74e52741744df67af13'
+      }
+    });
     console.log("onSearchSubmit", term);
   }
   render() {
     return (
       <div>
-        {/* {this.componentDidUpdate()} */}
+        {this.componentDidUpdate()}
         <section className="hero background-home is-link is-fullheight-with-navbar">
           <div className="container">
             <Header />
@@ -49,6 +84,7 @@ class App extends React.Component {
               <Hero />
               <div className="container">
                 <SearchBar onSubmit={this.onSearchSubmit}> </SearchBar>
+                <div className="container"> </div>
               </div>
             </section>
           </div>
